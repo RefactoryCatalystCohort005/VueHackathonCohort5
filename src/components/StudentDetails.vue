3d258481384bc9a6b9522210c7d0f8781fa84117
Wabuluka <dwabuluka@gmail.com>
2020-08-08 11:26:15 +0300

    html

---
+++ src/components/StudentDetails.vue
@@ -0,0 +1,25 @@
+ <template>
+  <div>
+    <img src alt />
+    <h4>NAME: </h4><span>Mwanje Mike</span>
+    <p>GENDER:</p><span>Male</span>
+    <p>AGE:</p><span>23</span>
+    <p>NATIONALITY:</p><span>Ugandan</span>
+    <p>ADDRESS:</p><span>Nakawa, Kampala</span>
+    <p>COHORT:</p><span>005</span>
+    <p>INTAKE:</p><span>001</span>
+    <p>EMAIL:</p><span>b.mwanje@student.ciu.ac.ug</span>
+    <p>CONTACT:</p><span>077 211 2233</span>
+
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StudentDetails",
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/StudentsView.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img src="" alt="">
+        <h4>Julius Wasajja</h4>
+        <span>Guild President At Refactory</span>
+        <a href="StudentDetails">
+            <button>VIEW PROFILE</button>
+        </a> 
+    </div>
+    
+</template>
+
+<script>
+import StudentDetails from './StudentDetails'
+export default {
+    name: 'StudentsView
+}
+</script>
+
+<style scoped>
+
+</style>
