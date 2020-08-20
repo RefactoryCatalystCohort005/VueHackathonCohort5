989636d678b922ca47e07fabeb494172cc0ff652
Mike Mwanje <mwanjemike767@gmail.com>
2020-08-20 20:39:29 +0300

    Home.vue

---
+++ src/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 id="app-headers">
+            <img alt="Vue logo" src="../assets/logo.png">
+            Cohort005 Student Profiling System<br />
+            <a class="btn btn-small">
+                <router-link to='/summary'> Summary </router-link>
+            </a>
+            <a class="btn btn-small">
+                <router-link to='/details'> Details </router-link>
+            </a>
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    components: {
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
