75091bd181d52b1c1e130b87b12d03203c2d223e
Mike Mwanje <mwanjemike767@gmail.com>
2020-08-21 16:59:59 +0300

    avatar and style
    Avatar file,
     and css matching requirement specification

--- src/components/StudentsView.vue
+++ src/components/StudentsView.vue
@@ -1,12 +1,12 @@
 <template>
     <div id="StudentsViewDiv">
         <div id="student-card" v-for="student in students" v-bind:key="student.studentId">
-            <img alt="Student Image" src="../assets/logo.png">
+            <img alt="Student Image" src="../../public/avatar.jpeg">
             <h4 id="name">{{student.name}}</h4>
             <span><i>{{student.role}}</i></span><br/><br />
-            <a href="StudentDetails">
-                <button>VIEW PROFILE</button>
-            </a>
+            <button>
+                <router-link to='/details'>VIEW PROFILE</router-link>
+            </button>
         </div> 
     </div>   
 </template>
@@ -17,12 +17,12 @@ export default {
     data(){
         return {
             students: [
-                {studentId: 'STD-001', name: 'Mike Mwanje', institute: 'Refactory', role: 'Student'},
-                {studentId: 'STD-002', name: 'Davies Wabuluka', institute: 'Refactory', role: 'Developer'},
-                {studentId: 'STD-003', name: 'Tsumaiya Nabawanuka', institute: 'Refactory', role: 'Applications Designer'},
-                {studentId: 'STD-004', name: 'Queen Emily', institute: 'Refactory', role: 'Class Representative'},
-                {studentId: 'STD-005', name: 'Isaac Semugenyi', institute: 'Refactory', role: 'Co-facilitator'},
-                {studentId: 'STD-006', name: 'Julius Wasajja', institute: 'Refactory', role: 'Gamer'},
+                {studentId: 'STD-001', name: 'Mike Mwanje', role: 'Student'},
+                {studentId: 'STD-005', name: 'Isaac Semugenyi', role: 'Co-facilitator'},
+                {studentId: 'STD-002', name: 'Davies Wabuluka', role: 'Developer'},
+                {studentId: 'STD-003', name: 'Tsumaiya Nabawanuka', role: 'Applications Designer'},
+                {studentId: 'STD-004', name: 'Queen Emily', role: 'Class Representative'},
+                {studentId: 'STD-006', name: 'Julius Wasajja', role: 'Gamer'},
             ]
         }
     }
@@ -34,7 +34,7 @@ export default {
         background-color: rgb(243, 244, 245);
         border-radius: 15px;
         box-shadow: inset 1px 1px 2px black;
-        min-width: 280px;
+        min-width: 400px;
         line-height: 1.5;
         padding:10px;
         text-align: center;
@@ -49,13 +49,10 @@ export default {
         font-weight: bold;
     }
     button {
-        display: flex;
-        justify-content:space-around;
         border-radius: 15px;
         font-weight: bold;
         color: rgb(243, 244, 245);
         background-color: rgb(58, 184, 173);
-        margin-left: 25%;
         margin-bottom: 50px;
     }
     span {
